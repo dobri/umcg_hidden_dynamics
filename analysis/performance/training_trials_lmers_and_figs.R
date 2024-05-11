f9c471e9d4f17b8a29399c175bdf5e189750bfc4
Dobromir Dotov <dobri.dotov@gmail.com>
2024-05-11 12:59:07 -0500

    ..

--- analysis/performance/training_trials_lmers_and_figs.R
+++ analysis/performance/training_trials_lmers_and_figs.R
@@ -2,11 +2,13 @@ library(janitor)
 library(tidyverse)
 library(ggplot2)
 library(ghibli)
-
+library(lme4)
+library(texreg)
 
 setwd('~/logos/umcg_hidden_dynamics/analysis/performance')
 # setwd('C:\\Users\\ddotov\\Downloads')
-x<-read.csv('Scores_2024-05-09.csv') %>%
+filename_scores_in <- 'Scores_2024-05-09.csv'
+x<-read.csv(filename_scores_in) %>%
   clean_names()
 x$training_phase <- sub("_", "", x$training_phase)
 x$training_phase <- sub("est_", "est", x$training_phase)
@@ -62,18 +64,22 @@ x$pp <- factor(x$pp)
 # One single weird trial.
 x <- x[!((x$training_phase == 'Retention') & (x$trial==6)),]
 
+# filename = paste(filename_scores_in,'cleaned',Sys.Date(),'.csv',sep='_')
+# write.csv(x, file=filename, row.names=FALSE)
+
 # Visualize
 colors<-ghibli_palette("PonyoMedium",7,type=("continuous"))[c(3,5,6)]
 colors[1]<-ghibli_palette("MarnieMedium2",7,type=("continuous"))[6]
 colors[2]<-ghibli_palette("MononokeMedium",7,type=("continuous"))[5]
 colors[3]<-ghibli_palette("YesterdayMedium",7,type=("continuous"))[6]
 
+sink("performance_scores_stats.txt")
+print(Sys.Date())
+sink()
 # Plot performance scores and stats ~ trial
 for (dv in c('score','c','pitch_error','tau')) {
 # for (dv in c('tau')) {
   for (task in c('Periodic Fixed','Periodic Fadeout (SCT)','Chaotic','Chaotic Visual')) {
-    print(paste('Test:',task,', DV:',dv))
-
     xs <- x[(x$task_label==task) & (x$training_phase!='Training'),]
     xs$dv <- xs[,dv]
 
@@ -82,24 +88,47 @@ for (dv in c('score','c','pitch_error','tau')) {
     if (dv=='pitch_error') {dv_lab = 'Pitch Error'}
     if (dv=='tau') {dv_lab = 'Anticipation'}
     
-    g<-ggplot(data=xs, aes(x=trial, y=dv, colour=Training)) +
-      facet_grid(~training_phase, scales="free_x") +
-      geom_jitter(size=1, alpha=.2, width=.1, height=0) +
-      #geom_line(aes(x=trial, y=0), col='black', size=1.2, alpha=.7) +
-      stat_summary(aes(x=trial, y=dv, colour=Training), fun='mean', geom='line', linewidth=2.2, alpha=1) +
-      stat_summary(aes(x=trial, y=dv, colour=Training), geom="ribbon", fun.data=mean_cl_boot, alpha=.5) +
-      theme_classic() +
-      theme(panel.background = element_rect(fill = "#111111",
-            colour = "#000000", linewidth = 1, linetype = "solid")) +
-      theme(legend.position="top") +
-            #legend.position="top", legend.title=element_blank()) +
-      #scale_x_continuous(breaks=seq(0,limit_lags,1), limits=c(0,limit_lags)) +
-      #scale_y_continuous(limits=c(-.6,.4)) +
-      labs(y = dv_lab) +
-      labs(x = "Trial") + 
-      scale_colour_manual(values=colors) + 
-      ggtitle(paste('Test task: ', task, sep=''))
-    print(g)
+    m1=lmer(dv ~ 1 + (1|pp),data=xs,REML=0)
+    m2=lmer(dv ~ 1 + trial + (1|pp),data=xs,REML=0)
+    m3=lmer(dv ~ 1 + trial + training_phase + (1|pp),data=xs,REML=0)
+    m4=lmer(dv ~ 1 + trial + training_phase + training_phase:Training + (1|pp),data=xs,REML=0)
+    sink("performance_scores_stats.txt", append = T)
+    cat("\n###########\n\n")
+    print(paste('Test: ',task,'; DV: ',dv,sep=''))
+    print(anova(m1,m2,m3,m4))
+    print(summary(m1))
+    print(summary(m2))
+    print(summary(m3))
+    print(summary(m4))
+    print(screenreg(list(m1,m2,m3,m4)))
+    sink()
+  
+    if (dv=='score') {xs$fitted <- getME(m4,'X') %*% fixef(m4)}
+    if (dv=='c') {xs$fitted <- getME(m4,'X') %*% fixef(m4)}
+    if (dv=='pitch_error') {xs$fitted <- getME(m4,'X') %*% fixef(m4)}
+    if (dv=='tau') {xs$fitted <- getME(m4,'X') %*% fixef(m4)}
+
+    if (TRUE){
+      g<-ggplot(data=xs, aes(x=trial, y=dv, colour=Training)) +
+        facet_grid(~training_phase, scales="free_x") +
+        geom_jitter(size=1, alpha=.2, width=.1, height=0) +
+        # geom_line(aes(x=trial, y=0), col='black', size=1.2, alpha=.7) +
+        # stat_summary(aes(x=trial, y=dv, colour=Training), fun='mean', geom='line', linewidth=2.2, alpha=1) +
+        # stat_summary(aes(x=trial, y=dv, colour=Training), geom="ribbon", fun.data=mean_cl_boot, alpha=.5) +
+        geom_line(aes(x=trial, y=fitted, colour=Training), linewidth=1.2, alpha=.8) +
+        theme_classic() +
+        theme(panel.background = element_rect(fill = "#111111",
+              colour = "#000000", linewidth = 1, linetype = "solid")) +
+        theme(legend.position="top") +
+              #legend.position="top", legend.title=element_blank()) +
+        #scale_x_continuous(breaks=seq(0,limit_lags,1), limits=c(0,limit_lags)) +
+        #scale_y_continuous(limits=c(-.6,.4)) +
+        labs(y = dv_lab) +
+        labs(x = "Trial") + 
+        scale_colour_manual(values=colors) + 
+        ggtitle(paste('Test task: ', task, sep=''))
+      print(g)
+    }
     
     if (TRUE){
       filename = paste("performance",dv,task,Sys.Date(),'.png',sep='_')
@@ -108,55 +137,3 @@ for (dv in c('score','c','pitch_error','tau')) {
     }
   }
 }
-
-
-### END
-g<-list('vector',4)
-counter = 0
-for (dv in c(2,3,5,4)){
-  
-  x$dv<-x[,dv]
-  
-  if (names(x)[dv]=='score') {dv_lab = 'Sync & Match Score [C/RMSE]'}
-  if (names(x)[dv]=='cmax') {dv_lab = 'C'}
-  if (names(x)[dv]=='tau') {dv_lab = 'τ'}
-  if (names(x)[dv]=='rmse') {dv_lab = 'RMSE'}
-  
-  m1=lmer(dv ~ 1 + (-1+trial|pp),data=x,REML=0)
-  m2=lmer(dv ~ 1 + trial + (-1+trial|pp),data=x,REML=0)
-  m3=lmer(dv ~ 1 + trial+task + (-1+trial|pp),data=x,REML=0)
-  m4=lmer(dv ~ 1 + trial*task + (-1+trial|pp),data=x,REML=0)
-
-  # if (dv==2) {x$fitted <- getME(m4,'X') %*% fixef(m4)} 
-  # if (dv==3) {x$fitted <- getME(m4,'X') %*% fixef(m4)}
-  # if (dv==4) {x$fitted <- getME(m3,'X') %*% fixef(m3)}
-  # if (dv==5) {x$fitted <- getME(m2,'X') %*% fixef(m2)}
-  if (dv==2) {x$fitted <- getME(m3,'X') %*% fixef(m3)}
-  if (dv==3) {x$fitted <- getME(m3,'X') %*% fixef(m3)}
-  if (dv==4) {x$fitted <- getME(m3,'X') %*% fixef(m3)}
-  if (dv==5) {x$fitted <- getME(m3,'X') %*% fixef(m3)}
-  
-  counter = counter + 1
-  g[[counter]] <- ggplot(data=x, aes(x=trial, y=dv, colour=as.factor(task))) +
-    geom_jitter(size=1, alpha=.5, width=.5, height=.1) +
-    geom_line(aes(x=trial, y=fitted, colour=task), size=1.2, alpha=.7) +
-    #geom_line(aes(x=trial, y=0), col='black', size=1.2, alpha=.7) +
-    theme_classic() +
-    theme(panel.background = element_rect(fill = "#111111",
-                                          colour = "#000000",size = 1, linetype = "solid")) +
-    theme(legend.position="top",legend.title=element_blank()) +
-    labs(y = dv_lab) +
-    labs(x = "Trial")
-  g[[counter]] <- g[[counter]] + scale_colour_manual(values=colors)
-}
-multiplot(plotlist=g,cols=4)
-
-if (FALSE){
-  filename=paste("perf_scores_training_with_lin_model",'_',Sys.Date(),'.png',sep='')
-  png(filename=filename,width=16,height=6,units="in",res=300)
-  multiplot(plotlist=g,cols=4)
-  dev.off()
-}
-
-
-
