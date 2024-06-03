48cac13b1a1aff5b31b1aff72d7cb2d379c6cd9d
dobri <dobri.dotov@yandex.com>
2024-06-02 19:40:20 -0500

    some lyap

---
+++ analysis/maxlyap/matl/load_raw_in_DATA_per_pp.m
@@ -0,0 +1,157 @@
+%{
+Loop through participant folders and trials to then process in batches.
+Group the trials in a structure with levels for participant. Some analysis 
+parameters will be constrained to be the same for all trials in the current
+batch. Also useful for parallel processing which is needed because the 
+analysis takes a lot of time.
++ optional:
+Instead of wasting computation time and allowing for error, we can set
+the embedding dimension based on our prior knowledge of the task dynamic.
+m=2 for pure oscillaton (phase oscillator) and m=3 for nonlinear, chaotic.
+The raw data is also pre-analyzed to get tau (for the embedding delay).
+%}
+
+% Prepare the data, arrange in the right format, get the conditions for the file name, etc..
+cd('~/biomech/projects/side_projects/umcg/data/raw_data/')
+clear
+pps = dir('PP*')';
+A = table('Size',[3000,14],'VariableTypes',["string","int8","int8","string",'int8','string','logical','double','int32','double','double','double','double','int8']);
+A.Properties.VariableNames = {'FileName','PP','Day','Date','TaskCode','TrainingPhase','Visual','TrialDuration','SamplesNum','Score','C','tau','PitchError','TrainingPhaseNumber'};
+DATA = cell(1,numel(pps));
+c = 0;
+ppn = 0;
+for p = pps
+    ppn = ppn + 1;
+    dag = [];
+    dag{1} = dir(fullfile(p.folder,p.name,'Dag1','trial_log*'))';
+    dag{2} = dir(fullfile(p.folder,p.name,'Dag2','trial_log*'))';
+    counter_trial = 0;
+    for d = 1:2
+        trial_counter = 0;
+        S = dir(fullfile(dag{d}(1).folder,'scores*'));
+        if isempty(S);continue;end % corrupt scores file
+        scores = readtable(fullfile(S(end).folder,S(end).name));
+        for s = 1:size(scores,1)
+            fname = scores.rawDataFile{s};
+            x = dlmread(fullfile(dag{d}(1).folder,fname),',',1,0);
+            t = x(:,1); % time
+            if(x(end,1)<=59);continue;end
+
+            % Trial conditions and parameters. Same as in performance loop
+            trial_counter = trial_counter + 1;
+            c = c+1;
+            A{c,1} = string(fname);
+            A{c,2} = str2double(regexp(fname,'((?<=pp).*(?=_d))','match','once'));
+            A{c,3} = str2double(regexp(fname,'(?<=_d)([^_]+)','match','once'));
+            A{c,4} = string(regexp(fname,'((?<=log-).*(?=_task))','match','once'));
+            A{c,5} = str2double(regexp(fname,'((?<=task).*(?=_aud))','match','once'));
+            if d == 1
+                if any(regexp(fname,'test'))
+                    if trial_counter < 30
+                        A{c,6} = "PreTest__";
+                        A{c,14} = -1;
+                    else
+                        A{c,6} = "PostTest_";
+                        A{c,14} = 1;
+                    end
+                else
+                    A{c,6} = "Training_";
+                    A{c,14} = 0;
+                end
+            else
+                A{c,6} = "Retention";
+                A{c,14} = 2;
+            end
+            A{c,7} = str2double(regexp(fname,'((?<=vis).*(?=_eps))','match','once'))==1;
+            A{c,8} = x(end,1);
+            A{c,9} = size(x,1);
+            A{c,10} = scores.score(s);
+            A{c,11} = scores.cmax(s);
+            A{c,12} = scores.tau(s);
+            A{c,13} = scores.rmse(s);
+
+            % Don't search but decide the embedding dimension: 2 or 3,
+            % depending on the task space (oscillator or nonlinear sys)
+            if A{c,5} >= 20
+                m_stim = 3;
+                m_pp = 3;
+            else
+                m_stim = 3.0;
+                m_pp = 3.0;
+            end
+
+            % Prepare for TE
+            if A{c,7} == 1 % visual modality condition.
+                x_stim = x(:,16); % the stimulus
+                x_user = x(:,17); % the participant
+            else
+                x_stim = x(:,11); % the stimulus
+                x_user = x(:,12); % the participant
+            end
+
+            % plot(t,x_stim,'-k');hold on
+            % plot(t,x_user,'-k')
+
+            % A little bit of pre-processing: smooth and detrend.
+            x_stim = smooth(x_stim,10,'sgolay');
+            x_user = smooth(x_user,10,'sgolay');
+            % linear detrend the participant movement because this can
+            % mess up several of the parameters (inf corr time).
+            b = [ones(size(x_user)) t]\x_user;
+            x_user = x_user - b(2)*t;
+
+            % Resample to an uniform timeframe.
+            Fs = 50;
+            [x_user, ~] = resample(x_user, t, Fs);
+            [x_stim, t] = resample(x_stim, t, Fs);
+
+            % Crop the first 10 seconds of the trial and the last 59+ seconds of the trial.
+            x_user = x_user((t>10) & (t<59),:);
+            x_stim = x_stim((t>10) & (t<59),:);
+            t = t((t>10) & (t<59),:); % Crop the last 59+ seconds of the trial!
+            
+            % plot(t,x_stim,'--b')
+            % plot(t,x_user,'--m')
+
+            % hold off
+
+            if sum(isnan(x_stim)) > 0 ; keyboard ; end
+
+
+            t = t - min(t); % for simplicity, shift back to zero.
+            fprintf('%10.0f%10.4f%10.0f\n',[min(t) max(t) numel(t)])
+
+
+            x_stim = (x_stim-mean(x_stim))./std(x_stim);
+            x_user = (x_user-mean(x_stim))./std(x_stim);
+
+            % Estimate tau even though for now we do not use it. :/
+            % On theory the optimal tau in PSR should be about a
+            % quarter of the average oscillation period for signals
+            % that are mostly oscillatory (narrow band). This tau will
+            % make the original signal and its delayed copy orthogonal.
+            % So, you don't even need to search for the best tau using
+            % computationally intensive methods, you just pass this value.
+            % I have the quarter period tau here but I don't actually
+            % use it. It's a good idea to find a way to pass it to
+            % TRENTOOL, skipping default methods and savings time.
+            tau_stim = tau_quarter_period(x_stim,t);
+            tau_pp = tau_quarter_period(x_user,t);
+
+            counter_trial = counter_trial + 1;
+            DATA{ppn}.trial{counter_trial} = [x x]';
+            DATA{ppn}.scores(counter_trial,:) = A{c,10:13};
+            DATA{ppn}.pp{counter_trial} = A{c,2};
+            DATA{ppn}.file_name{counter_trial} = A{c,1};
+            DATA{ppn}.conditions(counter_trial,:) = A{c,[5 7 14]};
+            DATA{ppn}.time{counter_trial} = t';
+            DATA{ppn}.fsample(counter_trial) = 1/mean(diff(t));
+            DATA{ppn}.label = {'Stim','User'};
+            DATA{ppn}.tau(:,counter_trial) = [tau_stim; tau_pp];
+            DATA{ppn}.dim(:,counter_trial) = [m_stim; m_pp];
+        end
+    end
+end
+A(c+1:end,:) = [];
+
+% save(['DATA_' char(datetime('now','Format','yyyy-MM-dd')) '.mat'],'DATA','A','-v7.3')
---
+++ analysis/maxlyap/matl/te_loop.m
@@ -0,0 +1,180 @@
+clear
+
+%% set software paths
+addpath('~/logos/umcg_hidden_dynamics/analysis/te/matl/')
+addpath('~/logos/umcg_hidden_dynamics/analysis/te/matl/libs/TRENTOOL3')
+addpath('~/logos/umcg_hidden_dynamics/analysis/te/matl/libs/fieldtrip');
+ft_defaults;
+
+%% define data paths
+base_folder = '~/biomech/projects/side_projects/umcg/data/';
+% base_folder = 'C:\Users\ddotov\biomech\projects\side_projects\umcg\data\';
+OutputDataPath = fullfile(base_folder,'results/');
+InputDataPath  = fullfile(base_folder,'raw_data/');
+
+cd(fullfile(base_folder))
+data_aggregated_already = 1;
+if data_aggregated_already
+    load(fullfile(InputDataPath,'DATA_2024-05-24.mat'))
+else
+    DATA = load_raw_in_DATA_per_task(InputDataPath);
+end
+
+
+%%
+TGA_results = cell(1,numel(DATA));
+%parfor task = 1:numel(TGA_results)
+for task = 1:numel(TGA_results)
+    data = DATA{task};
+
+    %% define cfg for TEprepare.m
+    cfgTEP = [];
+
+    cfgTEP.toi                 = [min(DATA{task}.time{1,1}),max(DATA{task}.time{1,1})]; % time of interest
+    cfgTEP.channel             = DATA{task}.label;
+
+    % scanning of interaction delays u
+    cfgTEP.predicttimemin_u    = 0;      % minimum u to be scanned
+    cfgTEP.predicttimemax_u    = 1000;	  % maximum u to be scanned
+    cfgTEP.predicttimestepsize = 200; 	  % time steps between u's to be scanned
+
+    % estimator
+    cfgTEP.TEcalctype  = 'VW_ds'; % use the new TE estimator (Wibral, 2013)
+
+    % ACT estimation and constraints on allowed ACT(autocorelation time)
+    cfgTEP.trialselect = 'no';
+    cfgTEP.actthrvalue = 10;   % threshold for ACT
+    cfgTEP.maxlag      = 10;
+    cfgTEP.minnrtrials = 1;   % minimum acceptable number of trials
+
+    % optimizing embedding
+    cfgTEP.optimizemethod = 'ragwitz';  % criterion used
+    cfgTEP.ragdim         = DATA{task}.dim(1,1):DATA{task}.dim(1,1)*3;       % criterion dimension
+    cfgTEP.ragtaurange    = [.25 .5];
+    cfgTEP.ragtausteps    = 5;         % steps for ragwitz tau steps
+    cfgTEP.repPred        = 1e3; % size(data.trial{1,1},2)*(1/2);
+    % cfgTESS.dim = DATA{task}.dim(1,1);
+    % cfgTESS.tau = DATA{task}.tau(1,1);
+
+    % kernel-based TE estimation
+    cfgTEP.flagNei = 'Mass';            % neigbour analyse type
+    cfgTEP.sizeNei = 4;                 % neigbours to analyse
+
+    % set the level of verbosity of console outputs
+    cfgTEP.verbosity = 'info_minor';
+    %cfgTEP.verbosity = 'none';
+
+    % define cfg for TE
+    cfgTESS = [];
+
+    % use individual dimensions for embedding
+    cfgTESS.optdimusage = 'maxdim';
+    % cfgTESS.optdimusage = 'indivdim';
+
+    % statistical and shift testing
+    cfgTESS.tail           = 1;
+    % cfgTESS.numpermutation = 'findDelay';
+    cfgTESS.numpermutation = 1e3;
+    cfgTESS.shifttest      = 'no';
+
+    cfgTESS.surrogatetype = 'trialshuffling'; % 'trialreverse','blockreverse2','swapneighbors';
+
+    % don't calculate MI additionally to TE
+    cfgTESS.MIcalc = 0;
+
+    % results file name
+    cfgTESS.fileidout = strcat(OutputDataPath,'out_');
+
+    %% TE compuation, scanning over specified values of u
+    TGA_results{task} = InteractionDelayReconstruction_calculate(cfgTEP,cfgTESS,data);
+
+
+    %% Null distribution from surrogate pairs
+    % # repetitions of re-analyzing the full data w/ shifted (surrogate) pairs.
+    TGA_results{task}.ntrials = size(TGA_results{task}.TEmat,2);
+    TGA_results{task}.trial_shuffle_surr_n_per_trial = 3;
+    TGA_results{task}.TEmat_sur2 = nan(numel(cfgTEP.channel),0);
+
+    for repeats = 1:TGA_results{task}.trial_shuffle_surr_n_per_trial
+        trials2 = randperm(TGA_results{task}.ntrials);
+        data2 = data;
+        for tr = 1:numel(trials2)
+            data2.trial{tr}(2,:) = data.trial{trials2(tr)}(2,:);
+        end
+
+        cfgTEP2 = cfgTEP;
+        cfgTEP2.predicttimemin_u    = min(TGA_results{task}.cfg.predicttime_u);      % minimum u to be scanned
+        cfgTEP2.predicttimemax_u    = max(TGA_results{task}.cfg.predicttime_u);	  % maximum u to be scanned
+        if cfgTEP2.predicttimemin_u == cfgTEP2.predicttimemax_u
+            cfgTEP2.predicttimemax_u = cfgTEP2.predicttimemin_u + 100;
+        end
+        cfgTEP2.predicttimestepsize = (cfgTEP2.predicttimemax_u - cfgTEP2.predicttimemin_u); 	  % time steps between u's to be scanned
+        cfgTEP2.ragdim = max(TGA_results{task}.cfg.dim);
+
+        TGAtemp = InteractionDelayReconstruction_calculate(cfgTEP2,cfgTESS,data2);
+        TGA_results{task}.TGA_surrogate_n{repeats} = TGAtemp;
+        TGA_results{task}.TEmat_sur2 = [TGA_results{task}.TEmat_sur2 TGAtemp.TEmat];
+        fprintf('%6.2f%% surrogates done.\n',repeats/TGA_results{task}.trial_shuffle_surr_n_per_trial*1e2)
+    end
+end
+
+for task = 1:numel(TGA_results)
+    % m = mean(TGA_results{task}.TEmat_sur2,2);
+    % sd = std(TGA_results{task}.TEmat_sur2,[],2);
+    m = mean(TGA_results{task}.TEmat_sur2(:));
+    sd = std(TGA_results{task}.TEmat_sur2(:));
+    TGA_results{task}.TEmat_rescaled = (TGA_results{task}.TEmat - m)./sd;
+
+    colorvec = hsv(2);
+    for d = 1:2
+        subplot(4,2,task)
+        [n,edges] = histcounts(TGA_results{task}.TEmat(d,:)','Normalization','probability');
+        [n0,edges0] = histcounts(TGA_results{task}.TEmat_sur2(d,:)','Normalization','probability');
+        plot(edges(2:end)+diff(edges(1:2))/2,n,'-','Color',colorvec(d,:).*.8,'linewidth',1.5)
+        hold on
+        plot(edges0(2:end)+diff(edges0(1:2))/2,n0,'--','linewidth',2,'Color',colorvec(d,:))
+    end
+    title(['Task ' num2str(DATA{task}.task{1})])
+    hold off
+    legend('TE_{Stim->Human}','TE_{Surrogate,Stim->Human}','TE_{Human->Stim}','TE_{Surrogate,Human->Stim}')
+    legend('boxoff')
+
+    DATA{task}.te = TGA_results{task}.TEmat_rescaled;
+end
+% f = fullfile(OutputDataPath,['PDF_TE_rescaled_' char(datetime('now','TimeZone','local','Format','y-MM-d-hhmmss')) '.jpeg']);
+% print('-djpeg','-r300',f)
+
+
+T = table('Size',[3000,14],'VariableTypes',{'string','int8','int8','string',...
+    'int8','string','string','int8',...
+    'double','double','double','double','double','double'});
+T.Properties.VariableNames = {'FileName','PP','Day','Date',...
+    'TaskCode','TrainingPhase','TrainingPhaseLabel','Visual',...
+    'Score','C','tau','PitchError','TE12rescaled','TE21rescaled'};
+counter = 0;
+for task = 1:numel(TGA_results)
+    for tr = 1:TGA_results{task}.ntrials
+        counter = counter + 1;
+        T{counter,1} = DATA{task}.trial_name{1,tr};
+        T{counter,2} = DATA{task}.pp{1,tr};
+        T{counter,3} = DATA{task}.day{1,tr};
+        T{counter,4} = DATA{task}.date{1,tr};
+        T{counter,5} = DATA{task}.task{1,tr};
+        T{counter,6} = DATA{task}.training_phase{1,tr};
+        T{counter,7} = DATA{task}.training_phase_label{1,tr};
+        T{counter,8} = DATA{task}.visual{1,tr};
+        T{counter,9} = DATA{task}.scores(1,tr);
+        T{counter,10} = DATA{task}.scores(2,tr);
+        T{counter,11} = DATA{task}.scores(3,tr);
+        T{counter,12} = DATA{task}.scores(4,tr);
+        T{counter,13} = TGA_results{task}.TEmat_rescaled(1,tr);
+        T{counter,14} = TGA_results{task}.TEmat_rescaled(2,tr);
+    end
+end
+T(counter+1:end,:) = [];
+
+
+%{
+save(fullfile(OutputDataPath,['out_TGA_results_' char(datetime('now','Format','yyyy-MM-dd')) '.mat']),'TGA_results','T','OutputDataPath','InputDataPath');
+writetable(T,fullfile(OutputDataPath,['Scores_TE_' char(datetime('now','Format','yyyy-MM-dd')) '.csv']))
+%}
